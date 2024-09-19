9e0003daa45a9bf24ade898fe1c2543a9ead3708
Nathan Emmerson <nathanemmerson07@gmail.com>
2024-09-20 09:56:56 +1200

    added rewards adding to user

--- server/routes/users.ts
+++ server/routes/users.ts
@@ -4,6 +4,7 @@ import { StatusCodes } from 'http-status-codes'
 import * as db from '../db/functions/users'
 import * as children from '../db/functions/children'
 import { AddChore } from '../db/functions/chores'
+import { addReward } from '../db/functions/rewards'
 
 const router = Router()
 
@@ -32,30 +33,56 @@ router.get('/:id', checkJwt, async (req, res) => {
   }
 })
 
-router.post('/', checkJwt, async (req,res) => {
+router.post('/', checkJwt, async (req: JwtRequest, res) => {
   try {
-    const { userId,name, createdAt,updatedAt } = req.body
-    const id = await children.addChildren({userId, name, createdAt, updatedAt})
-    res.setHeader('addChild', `${req.baseUrl}/${id}`).sendStatus(StatusCodes.CREATED)
-
+    const { userId, name, createdAt, updatedAt } = req.body
+    const id = await children.addChildren({
+      userId,
+      name,
+      createdAt,
+      updatedAt,
+    })
+    res
+      .setHeader('addChild', `${req.baseUrl}/${id}`)
+      .sendStatus(StatusCodes.CREATED)
   } catch (err) {
     console.log(err)
   }
 })
 
-router.post('/', checkJwt, async (req, res) => {
+router.post('/', checkJwt, async (req: JwtRequest, res) => {
   try {
-    const {name, description, frequency, createdAt, updatedAt} = req.body
-    const id = await AddChore({name,description,frequency,createdAt,updatedAt})
-    res.setHeader('addchore', `${req.baseUrl}/${id}`).sendStatus(StatusCodes.CREATED)
-  }catch (err) {
+    const { name, description, frequency, createdAt, updatedAt } = req.body
+    const id = await AddChore({
+      name,
+      description,
+      frequency,
+      createdAt,
+      updatedAt,
+    })
+    res
+      .setHeader('addchore', `${req.baseUrl}/${id}`)
+      .sendStatus(StatusCodes.CREATED)
+  } catch (err) {
     console.log(err)
   }
 })
 
-router.post('/' checkJwt, async (req,res) => {
+router.post('/', checkJwt, async (req: JwtRequest, res) => {
   try {
-    const 
+    const { name, description, pointsRequired, createdAt, updatedAt } = req.body
+    const id = await addReward({
+      name,
+      description,
+      pointsRequired,
+      createdAt,
+      updatedAt,
+    })
+    res
+      .setHeader('Add Reward', `${req.baseUrl}/${id}`)
+      .sendStatus(StatusCodes.CREATED)
+  } catch (err) {
+    console.log(err)
   }
 })
 
