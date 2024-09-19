c24d575d4b874614b3a13f46965b04c4fa891ad7
Nathan Emmerson <nathanemmerson07@gmail.com>
2024-09-19 14:37:10 +1200

    added routes function

---
+++ server/routes/users.ts
@@ -0,0 +1,33 @@
+import { Router } from 'express'
+import checkJwt, { JwtRequest } from '../auth0'
+import { StatusCodes } from 'http-status-codes'
+import * as db from '../db/functions/users'
+
+const router = Router()
+
+router.use(checkJwt)
+
+router.get('/', async (req, res) => {
+  try {
+    const user = await db.getAllUser()
+    res.json(user)
+  } catch (error) {
+    console.log(error)
+    res
+      .status(500)
+      .json({ messege: 'something went wrong with  getting users' })
+  }
+})
+
+router.get('/:id', async (req, res) => {
+  try {
+    const id = Number(req.params)
+    const user = await db.getUserById(id)
+    res.json(user)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ messege: 'Error getting user By id' })
+  }
+})
+
+// router.post('/', checkJwt, async (req: JwtRequest, res, ))
